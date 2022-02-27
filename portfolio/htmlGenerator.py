3b5d22a28d0c835558ef4af6081fabae89c64aaa
Smart Chen <30527189+SmartChen2005@users.noreply.github.com>
2022-02-27 11:12:07 +0800

    none

--- portfolio/htmlGenerator.py
+++ portfolio/htmlGenerator.py
@@ -2,10 +2,9 @@ from exif import Image
 from fractions import Fraction
 import os
 
-
 # https://exif.readthedocs.io/en/latest/
 
-projectName =input("Enter Project Name:")
+projectName = input("Enter Project Name:")
 directory = f'/Users/smartchen/Documents/GitHub/smartchen2005.github.io/portfolio/{projectName}/img'
 
 
@@ -17,13 +16,14 @@ def getEXIF(fileName):
     date = img.datetime[0:10].split(":")
     return [name, img.model, img.lens_model, "f" + str(img.f_number),
             str(Fraction(img.exposure_time).limit_denominator(10000)) + 's',
-            "iso" + str(img.photographic_sensitivity), date[0]+'/'+date[1]+'/'+date[2]]
+            "iso" + str(img.photographic_sensitivity), date[0] + '/' + date[1] + '/' + date[2]]
 
 
 def printAllPicsInfo(directory):
     for file in os.listdir(directory):
         if file[len(file) - 3:len(file)] == 'jpg' or file[len(file) - 4:len(file)] == 'jpeg':
             print(getEXIF(directory + '/' + file))
+
 
 # printAllPicsInfo(directory)
 
@@ -35,8 +35,7 @@ fileList.sort()
 
 for file in fileList:
     info = getEXIF(directory + "/" + file)
-    if info[1][0:6] == "iPhone":
-        info[2] = ""
+    if info[1][0:6] == "iPhone" and file.split(".")[0][-1] != "L":
         print(f"""
 <div class="polaroid">
     <img src="img/{file}" style="width:100%">
@@ -44,11 +43,27 @@ for file in fileList:
         <p>{info[1]}<br>{info[3]} {info[4]} {info[5]}<br>{info[6]}</p>
     </div>
 </div>""")
-    else:
+    elif info[1][0:6] == "iPhone" and file.split(".")[0][-1] == "L":
+        print(f"""
+<div class="polaroid-large">
+    <img src="img/{file}" style="width:100%">
+    <div class="description">
+        <p>{info[1]}<br>{info[3]} {info[4]} {info[5]}<br>{info[6]}</p>
+    </div>
+</div>""")
+    elif file.split(".")[0][-1] != "L":
         print(f"""
 <div class="polaroid">
     <img src="img/{file}" style="width:100%">
     <div class="description">
         <p>{info[1]}<br>{info[2]}<br>{info[3]} {info[4]} {info[5]}<br>{info[6]}</p>
+    </div>
+</div>""")
+    else:
+        print(f"""
+<div class="polaroid-large">
+    <img src="img/{file}" style="width:100%">
+    <div class="description">
+            <p>{info[1]}<br>{info[2]}<br>{info[3]} {info[4]} {info[5]}<br>{info[6]}</p>
     </div>
 </div>""")
